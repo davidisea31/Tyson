3aa59d05c4b84f927c2d38d1548afcbdea20e2c0
David Isea <112912252+davidisea31@users.noreply.github.com>
2023-10-10 21:44:47 -0500

    Update tate.js

--- tate.js
+++ tate.js
@@ -1,18 +1,6 @@
-
-                document.addEventListener("DOMContentLoaded", function () {
-                    const formulario = document.getElementById("formulario-comentario");
-            
-                    formulario.addEventListener("submit", function (event) {
-                        event.preventDefault();
-            
-                        // Aquí puedes agregar código para validar los campos del formulario si es necesario
-            
-                        // Simulación de envío AJAX (reemplazar con la lógica real)
-                        setTimeout(function () {
-                            alert("Comentario enviado con éxito"); // Mostrar un mensaje de éxito
-                            formulario.reset(); // Limpiar el formulario
-                        }, 1000);
-                    });
-                });
+document.getElementById("menu-btn").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+});
         
 
